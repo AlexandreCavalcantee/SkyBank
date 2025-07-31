0574c0827035241d939137fdd42bfa76cf51d9a7
Alexandre Cavalcante <alexandrecavalcante.contato@gmail.com>
2025-07-31 17:02:21 -0300

    first commit

---
+++ ContaBancaria.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class ContaBancaria {
+    public static void main(String[] args) {
+        System.out.println("Bem vindo ao SkyBank oque deseja realizar?");
+        Scanner sc = new Scanner(System.in);
+    }
+}
